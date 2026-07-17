c01e6eb59525609fd765d8de2024f458add51504
GustavoLVJardim <gustavolvjardim@gmail.com>
2026-07-17 20:51:01 +0100

    Quarto commit: inclui a função para remoção do db e novo menu de escolha

--- main.py
+++ main.py
@@ -7,21 +7,35 @@ def main():
     print("--------- Funcionários - Banco de Dados -----------")
     print("===================================================")
     print()
-    escolha = input("Deseja realizar algum cadastro de funcinário?[s/n]: ").strip().lower()[0]
-    while True:
-        if escolha == "s":
-            funcionario = faz_cadastro()
-            mostra_cadastro_prévio(funcionario)
-            print()
-            print("Vamos fazer a inserção em nosso banco de dados...")
-            insere_dados_funcionario_na_tabela(funcionario)
-            print("Aguarde um instante...")
-            time.sleep(2)
-            print("Dados salvos com sucesso. Muito Obrigado!")
-            break
-        else:
-            print("obrigado")
-            break
+    escolha = input("""
+Digite 1 para realizar algum cadastro de funcinário: 
+Digite 2 para realizar a remoção de cadastro de funcionário: 
+Digite 3 para sair: """).strip()[0]
+    match escolha:
+        case "1":
+            while True:
+                if escolha == "s":
+                    funcionario = faz_cadastro()
+                    mostra_cadastro_prévio(funcionario)
+                    print()
+                    print("Vamos fazer a inserção em nosso banco de dados...")
+                    insere_dados_funcionario_na_tabela(funcionario)
+                    print("Aguarde um instante...")
+                    time.sleep(2)
+                    print("Dados salvos com sucesso. Muito Obrigado!")
+                    break
+                else:
+                    print("obrigado")
+                    break
+        
+        case "2":
+            esquecer()
+        
+        case "3":
+            exit()
+        
+        case _:
+            print("opção inválida")
 
 if __name__ == "__main__":
     main()
--- script_sql.py
+++ script_sql.py
@@ -1,18 +1,14 @@
 import sqlite3
-
-
 def cria_conexao_ou_tabela_sql():
     conexao = sqlite3.connect("empresa.db")
     cursor = conexao.cursor()
-
     script_sql = """
         CREATE TABLE IF NOT EXISTS funcionarios (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nome TEXT NOT NULL,
         email TEXT NOT NULL,
         departamento TEXT NOT NULL
-    );
-    """
+    );"""
     cursor.execute(script_sql)
     conexao.commit()
     conexao.close()
@@ -21,16 +17,32 @@ def insere_dados_funcionario_na_tabela(tupla_de_funcionario):
     try:
         conexao = sqlite3.connect("empresa.db")
         cursor = conexao.cursor()
-
         script_sql = """
         INSERT INTO funcionarios (nome, email, departamento)
-        VALUES(?,?,?);         
-        """
+        VALUES(?,?,?);"""
         cursor.execute(script_sql, tupla_de_funcionario)
         conexao.commit()
 
     except sqlite3.Error as erro:
         print(f"\n[ERRO SQL]: Não foi possível inserir os dados. DETALHE: {erro}")
+        
+    finally:
+        conexao.close()
+
+def esquecer():
+    try:
+        id_para_esquecer = input("Digite o id para remoção: ")
+        conexao = sqlite3.connect("empresa.db")
+        cursor = conexao.cursor()
+        script_sql = F"""
+        DELETE FROM funcionarios
+        WHERE id = {id_para_esquecer}"""
+        cursor.execute(script_sql)
+        conexao.commit()
+        print("A remoção completa foi realizada com sucesso")
+
+    except sqlite3.Error as erro:
+        print(f"\n[ERRO SQL]: Não foi possível remover os dados. DETALHE: {erro}")
         
     finally:
         conexao.close()
